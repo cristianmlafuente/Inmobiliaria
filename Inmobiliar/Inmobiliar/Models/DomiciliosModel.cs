8d93daf5c4386ad1c36a0b5e29cbf57068c2b8c7
Cristian Miguel Lafuente <clafuente@proveedor.personal.com.ar>
2017-08-02 18:06:50 -0300

    Cambios para el registro de la propiedad

---
+++ Inmobiliar/Inmobiliar/Models/DomiciliosModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Inmobiliar.Models
+{
+    public class DomiciliosModel
+    {
+        public int IdAddress { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string Calle { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string Ciudad { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string CP { get; set; }
+        public Nullable<int> Piso { get; set; }
+        public string Dto { get; set; }
+        public Nullable<int> Numero { get; set; }
+        [Required]
+        public string Barrio { get; set; }
+    }
+}
--- Inmobiliar/Inmobiliar/Models/PropiedadesModel.cs
+++ Inmobiliar/Inmobiliar/Models/PropiedadesModel.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Inmobiliar.Models
 {
     public class PropiedadesModel
     {
-        public string Direccion { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public DomiciliosModel domicilio { get; set; }
         public string Tipo { get; set; }
-        public string Dueño { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public PersonasModel Dueño { get; set; }
+        public Nullable<int> NroContratoEpec { get; set; }
+        public string NomenclaturaCatastral { get; set; }
+        public string NumeroCtaRenta { get; set; }
+        public Nullable<int> UnidadFacturacion { get; set; }
     }
 }
