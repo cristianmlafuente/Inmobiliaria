dc1b78ff335e880093ce4b3c0a8f75a89bb95667
Guillermo Armando Correa <gcorrea@proveedor.personal.com.ar>
2017-08-14 19:47:41 -0300

    Agrego pago

---
+++ Inmobiliar/InmBLL/AdministradoraAlquileres.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.OleDb;
+using InmBLL.Entities;
+using System.IO;
+
+namespace InmBLL
+{
+    public class AdministradoraAlquileres
+    {
+        #region "Recibos Alquileres"
+
+        public void RegistrarPagoAlquiler()
+        {
+
+        }
+
+        public byte[] GenerarRecibo(PagoAlquiler Pago)
+        {
+            OleDbConnection conexion = null;
+            string PATH_DIRECTORIO_ARCHIVOS = ConfigurationManager.AppSettings["PathArchivosOriginal"];
+            string ReciboAlquiler = ConfigurationManager.AppSettings["ArchivoReciboAlquiler"];
+            string PATH_DIRECTORIO_TEMP = ConfigurationManager.AppSettings["PathArchivosTemp"];
+            try
+            {
+
+                string ArchivoOriginalRecibo = PATH_DIRECTORIO_ARCHIVOS + ReciboAlquiler;
+                string ArchivoFinalRecibo = PATH_DIRECTORIO_TEMP + ReciboAlquiler.Split('.')[0] + Pago.Periodo.ToString("MMyy") + Pago.idInquilino.ToString() + Pago.idPropiedad.ToString() + "." + ReciboAlquiler.Split('.')[1];
+                if (File.Exists(ArchivoFinalRecibo))
+                    File.Delete(ArchivoFinalRecibo);
+                File.Copy(ArchivoOriginalRecibo, ArchivoFinalRecibo);
+                string cadenaConexion = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + ArchivoFinalRecibo + ";Extended Properties='Excel 12.0 Xml;HDR=NO';";
+                using (conexion = new OleDbConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    using (OleDbCommand comando = conexion.CreateCommand())
+                    {
+
+
+
+                    }
+
+                    conexion.Close();
+                    conexion.Dispose();
+                    conexion = null;
+                }
+
+
+                var arr = new byte[40];
+                return arr;
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception(ex.Message);
+            }            
+        }
+
+        #endregion
+    }
+}
+
---
+++ Inmobiliar/InmBLL/Entities/PagoAlquiler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InmDAL;
+
+namespace InmBLL.Entities
+{
+    public class PagoAlquiler
+    {
+        public DateTime Periodo { get; set; }
+        public DateTime FechaPago { get; set;}
+        public int idInquilino { get; set; }
+        public int idPropiedad { get; set; }
+        private InmDAL.Propiedades _propiedad;
+        private InmDAL.Personas _inquilino;
+
+        public ReadOnly Propiedades Propiedad
+        {
+            get
+            {        
+                if (_propiedad == null)
+                {
+                    if (idPropiedad != 0)
+                    {
+                        _propiedad = new InmDAL.Propiedades();
+                        return _propiedad;
+                    }
+                    else return null;
+                }
+                else return _propiedad;
+            }
+        }
+
+        public ReadOnly InmDAL.Personas Inquilino
+        {
+            get
+            {
+                if (_inquilino == null)
+                {
+                    if (idInquilino != 0)
+                    {
+                        _inquilino = new InmDAL.Personas();
+                        return _inquilino;
+                    }
+                }
+                else
+                    return _inquilino;
+            }            
+        }
+
+    }
+}
