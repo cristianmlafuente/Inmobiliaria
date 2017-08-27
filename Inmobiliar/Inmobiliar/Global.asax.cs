82557533de668363b0e8ee7dd3c3fb8664167b0f
Guillermo Armando Correa <gcorrea@proveedor.personal.com.ar>
2017-08-27 18:18:44 -0300

    add unity

--- Inmobiliar/Inmobiliar/Global.asax.cs
+++ Inmobiliar/Inmobiliar/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using InmoIoC.Services.Configuration;
 
 namespace Inmobiliar
 {
@@ -16,6 +18,9 @@ namespace Inmobiliar
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var _configuration = new ConfigurationIoCAPI();
+            var _container = _configuration.ConfigurationIoC();
+            var _dependencyResolver = new Inmobiliar.Resolvers.UnityDependencyResolver(_container);
         }
     }
 }
---
+++ Inmobiliar/Inmobiliar/Resolvers/UnityDependencyResolver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Microsoft.Practices.Unity;
+
+namespace Inmobiliar.Resolvers
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer unity;
+        private const string CONTROLLER_FACTORY_TYPE = "system.web.mvc.icontrollerfactory";
+        private const string CONTROLLER_ACTIVATOR_TYPE = "system.web.mvc.icontrolleractivator";
+        private const string MODEL_METADATA_PROVIDER_TYPE = "system.web.mvc.modelmetadataprovider";
+        private const string PAGE_ACTIVATOR_TYPE = "system.web.mvc.iviewpageactivator";
+
+
+        public UnityDependencyResolver(IUnityContainer unity)
+        {
+            this.unity = unity;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                var service = this.unity.Resolve(serviceType);
+                return service;
+            }
+            catch (Exception ex)
+            {
+                LogException(ex, serviceType);
+                // By definition of IDependencyResolver contract, this should return null if it cannot be found.
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return this.unity.ResolveAll(serviceType);
+            }
+            catch (Exception ex)
+            {
+                LogException(ex, serviceType);
+                // By definition of IDependencyResolver contract, this should return null if it cannot be found.
+                return null;
+            }
+        }
+
+
+        private static void LogException(Exception ex, Type serviceType)
+        {
+            string serviceTypeName = serviceType.ToString().ToLower();
+
+            switch (serviceTypeName)
+            {
+                case CONTROLLER_FACTORY_TYPE: break;
+                case CONTROLLER_ACTIVATOR_TYPE: break;
+                case MODEL_METADATA_PROVIDER_TYPE: break;
+                case PAGE_ACTIVATOR_TYPE: break;
+                default:
+                    // Log Exception
+                    break;
+            }
+        }
+
+        public void Dispose()
+        {
+            // When BeginScope returns 'this', the Dispose method must be a no-op.
+        }
+    }
+}
